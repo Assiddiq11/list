7a2b6b2931656d81b294891f1bb3ec5a8b2f9b31
Assiddiq11 <abubakarmahmoudmaina@gmail.com>
2024-10-17 19:08:38 +0100

    Add files via upload

---
+++ Shopping.py
@@ -0,0 +1,22 @@
+shopping_for_clothes = ["Shoes","Shirts","Trousers"]
+print (shopping_for_clothes)
+
+print(shopping_for_clothes[1])
+
+shopping_for_clothes.append("Hoodies")
+print(shopping_for_clothes)
+
+shopping_for_clothes.remove("Trousers")
+print(shopping_for_clothes)
+
+print(shopping_for_clothes[1:3])
+
+
+matrix = [[1,2,3] , [4,5,6] , [7,8,9]]
+print(matrix)
+
+print(matrix[2][0])
+
+rows = int(input("Enter the number of rows"))
+columns = int(input("Enter the number of columns"))
+print(matrix)
